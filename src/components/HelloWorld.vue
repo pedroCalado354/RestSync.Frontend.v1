49f58e057e31cd0fd15b656be3119897ef00faf1
pedrocalado354 <pedro.calado@emotionmobility.com>
2024-11-19 09:12:30 +0000

    second commit for testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      já aspiraste a casa hoje?
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
